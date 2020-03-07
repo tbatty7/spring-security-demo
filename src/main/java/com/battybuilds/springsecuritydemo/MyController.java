00bcbe6bbe68a25bb1a393f7efbe1c7040b41467
Tim B <tbatty7@gmail.com>
2020-03-06 20:58:29 -0500

    initial commit with spring security and user and password defined

---
+++ src/main/java/com/battybuilds/springsecuritydemo/MyController.java
@@ -0,0 +1,22 @@
+package com.battybuilds.springsecuritydemo;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+
+    @PostMapping("/request")
+    public ResponseEntity receiveRequest(@RequestBody String body) {
+        System.out.println("%%%%%%%%%%%%%%%% body: " + body);
+        return ResponseEntity.ok("\nhi");
+    }
+
+    @GetMapping("/check")
+    public String getInfo() {
+        return "<h1>Got it!</h1>";
+    }
+}
