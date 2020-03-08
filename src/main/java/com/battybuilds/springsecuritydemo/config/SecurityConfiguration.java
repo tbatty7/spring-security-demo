3a76ed4b54250119871db02efb1da506bbb7e232
Tim B <tbatty7@gmail.com>
2020-03-07 19:49:58 -0500

    converted to jdbc authentication with a SQL database to store users/pwds

--- src/main/java/com/battybuilds/springsecuritydemo/config/SecurityConfiguration.java
+++ src/main/java/com/battybuilds/springsecuritydemo/config/SecurityConfiguration.java
@@ -1,26 +1,50 @@
 package com.battybuilds.springsecuritydemo.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import javax.sql.DataSource;
+
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private DataSource datasource; // Spring automatically configures to h2 database as default
+
+//    This is for Basic In-Memory authentication
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        // Authentication
+//        auth.inMemoryAuthentication()
+//        .withUser("admin")
+//        .password("admintim")
+//        .roles("ADMIN")
+//        .and()
+//        .withUser("user")
+//        .password("awesome")
+//        .roles("USER");
+//    }
+
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        // Authentication
-        auth.inMemoryAuthentication()
-        .withUser("admin")
-        .password("admintim")
-        .roles("ADMIN")
-        .and()
-        .withUser("user")
-        .password("awesome")
-        .roles("USER");
+        auth.jdbcAuthentication()
+                .dataSource(datasource)
+                .withDefaultSchema()  // Spring automatically creates the Auth tables in database with this
+                .withUser(
+                        User.withUsername("admin").password("admintim").roles("ADMIN")
+                )
+                .withUser(
+                        User.withUsername("user").password("awesome").roles("USER")
+                );
+
     }
 
     @Override
